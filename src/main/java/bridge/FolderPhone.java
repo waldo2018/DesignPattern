ce84ec0dcfb7d302214e5bd21bce2b19db239eae
waldo2018 <wanghaichao2012@gmail.com>
2020-11-22 08:54:28 +0900

    桥接模式学习

---
+++ src/main/java/bridge/FolderPhone.java
@@ -0,0 +1,27 @@
+package bridge;
+
+import java.sql.SQLOutput;
+
+public class FolderPhone extends Phone{
+    public FolderPhone(Brand brand) {
+        super(brand);
+    }
+
+    @Override
+    protected void open() {
+        super.open();
+        System.out.println("折叠样式手机");
+    }
+
+    @Override
+    protected void close() {
+        super.close();
+        System.out.println("折叠样式手机");
+    }
+
+    @Override
+    protected void call() {
+        super.call();
+        System.out.println("折叠样式手机");
+    }
+}
